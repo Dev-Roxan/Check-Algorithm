5e84cd033f00e89fdbc0b32009ca1c2615e7eb3a
Roxan Teves <roxanteves@mlhuillier1.com>
2015-06-22 15:28:13 +0800

    with 4partner's Algo
    globe, innove, asialink, veco

---
+++ Version1/CheckDigitAlgo/AlgoTester/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+using CheckDigitAlgo;
+
+namespace AlgoTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            object result = null;
+
+            if(comboBox1.SelectedItem.Equals("Innove"))
+            {
+                result =  Globe.CheckDigitInnove(Convert.ToInt32(textBox1.Text));
+            }
+            if(comboBox1.SelectedItem.Equals("Globe Handy"))
+            { 
+                result = Globe.CheckDigitGlobeHandy(Convert.ToInt32(textBox1.Text));
+            }
+            if (comboBox1.SelectedItem.Equals("Asialink"))
+            {
+                result = Globe.CheckAsialink(textBox1.Text);
+            }
+            if (comboBox1.SelectedItem.Equals("VECO"))
+            {
+                result = Globe.CheckDigitVeco(textBox1.Text);
+            }   
+
+            MessageBox.Show(result.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Innove");
+            comboBox1.Items.Add("Globe Handy");
+            comboBox1.Items.Add("Asialink");
+            comboBox1.Items.Add("VECO");
+        }
+    }
+}
